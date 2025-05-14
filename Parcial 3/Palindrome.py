23937939bb2096f91d31485011366bc1756db436
MarioJV <marshal@Marios-MacBook-Pro.local>
2025-05-14 10:45:04 -0600

    Palindrome Number Added

---
+++ Parcial 3/Palindrome.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        numbers = list(str(x))
+
+        if numbers == numbers[::-1]:
+            return True
+        else: 
+            return False
